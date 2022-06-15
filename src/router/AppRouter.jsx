d2c762b8e0cb99a4cabf6e66ab8d96cb77c1a0f0
Luis Angel <lang@zuntrix.com>
2022-06-14 21:26:50 -0500

    First Commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+        {/* Login y Registro  */}
+        <Route />
+
+        {/* Journal App */}
+        <Route />
+    </Routes>
+  )
+}
