7805847ae151f09e4faa18f9f54a0bd5590cbed7
LuisMoov <lang@zuntrix.com>
2022-06-15 17:09:36 -0500

    "Commit desde Moov - Creacion de rutas"

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { AppRouter } from './router/AppRouter'
 
 export const JournalApp = () => {
   return (
     <>
-        <h1>Journal App test git From work</h1>
+        <AppRouter />
     </>
   )
 }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
+import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { JournalRoutes } from "../journal/routes/JournalRoutes";
 
 export const AppRouter = () => {
   return (
     <Routes>
         {/* Login y Registro  */}
-        <Route />
+        <Route path="/auth/*" element={<AuthRoutes />} />
 
         {/* Journal App */}
-        <Route />
+        <Route path="/*" element={ <JournalRoutes />} />
     </Routes>
   )
 }
