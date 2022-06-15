568414544e21b9ffc65393c127deb68f08594572
LuisMoov <lang@zuntrix.com>
2022-06-15 07:19:35 -0500

    "Ejemplo para push desde work"

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const JournalApp = () => {
   return (
     <>
-        <h1>Journal App test git</h1>
+        <h1>Journal App test git From work</h1>
     </>
   )
 }
