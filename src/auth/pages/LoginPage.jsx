23a8912bb6643699028645a292f3e11a5833e07b
LuisMoov <lang@zuntrix.com>
2022-08-26 15:51:03 -0500

    Config AuthSlice lección 271

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,33 +2,53 @@ import { Link as RouterLink } from "react-router-dom";
 import { Button, Grid, TextField, Typography, Link } from "@mui/material"
 import { Google } from "@mui/icons-material";
 import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from "../../hooks";
 
 export const LoginPage = () => {
+
+  const { email, password, onInputChange } = useForm({
+    email: 'Luis@google.com',
+    password: '123456'
+  });
+
+  const onSubmit = ( event ) => {
+    event.preventDefault();
+    console.log({ email, password });
+  }
+
   return (
     <AuthLayout title="Login">
 
-        <form>
+        <form onSubmit={ onSubmit }>
           <Grid container>
             <Grid item xs={12} sx={{mt:2}}>
               <TextField
-              label='Correo'
-              type='email'
-              placeholder="correo@mail.com"
-              fullWidth />
+                label='Correo'
+                type='email'
+                placeholder="correo@mail.com"
+                fullWidth
+                name = 'email'
+                value = { email }
+                onChange= { onInputChange }
+              />
             </Grid>
 
             <Grid item xs={12} sx={{mt:2}}>
               <TextField
-              label='Contraseña'
-              type='password'
-              placeholder="Contraseña"
-              fullWidth />
+                label='Contraseña'
+                type='password'
+                placeholder="Contraseña"
+                fullWidth
+                name = 'password'
+                value = { password }
+                onChange= { onInputChange }
+              />
             </Grid>
 
             <Grid container spacing={2} sx={{mb:2, mt:1}}>
 
               <Grid item xs={12} sm={6}>
-                <Button variant="contained" fullWidth>
+                <Button type="submit" variant="contained" fullWidth>
                   Login
                 </Button>
               </Grid>
